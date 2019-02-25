06b85c851ad3026b4b235acd046a6b6a1345d797
nilscharlois <nils.charlois.charlois@gmail.com>
2019-02-25 22:59:16 +0000

    Lecture 74 done

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.post('/todos', (req, res)=>{
   todo.save().then((doc)=>{
     res.send(doc)
   }, (err)=>{
-    res.status(400).send(e);
+    res.status(400).send(err);
   });
 });
 
@@ -30,3 +30,5 @@ app.post('/todos', (req, res)=>{
 app.listen(3000, ()=>{
   console.log('Started on port 3000');
 })
+
+module.exports = {app};
