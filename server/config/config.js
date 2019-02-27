e15b73faacf1ad4ee48a63f285bc0aa5f54b2548
nilscharlois <nils.charlois@gmail.com>
2019-02-27 13:10:55 +0000

    Lecture 86

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+var env = process.env.NODE_ENV;
+
+console.log('environment ',env);
+
+// switch the target DB based on the environment
+if(env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
+// 3 environments
+// test
+// dev
+// production
+
+process.env.NODE_ENV === 'production'
